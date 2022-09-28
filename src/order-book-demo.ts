75b438c0d9d1ae9fbd24cf7a4a7f255850569cf6
Roy <hfers@msn.com>
2022-09-28 15:19:54 +0800

    add decreaseOrder handler

--- src/order-book-demo.ts
+++ src/order-book-demo.ts
@@ -18,7 +18,7 @@ import {
     UpdateMinPurchaseTokenAmountUsd,
     UpdateSwapOrder
 } from "../generated/OrderBookDemo/OrderBookDemo"
-import {ExampleEntity, Account, Order, Transaction} from "../generated/schema"
+import {ExampleEntity, Account, Order, DeOrder, Transaction} from "../generated/schema"
 
 // handle IncreaseOrder
 export function handleCreateIncreaseOrder(event: CreateIncreaseOrder): void {
@@ -28,6 +28,7 @@ export function handleCreateIncreaseOrder(event: CreateIncreaseOrder): void {
         account.address = event.params.account.toHexString()
         account.lastOrderIndex = event.params.orderIndex
         account.orders = []
+        account.deOrders = []
 
         let newOrdersList = account.orders
         //use address concat lastOrderIndex as uuid
@@ -54,7 +55,6 @@ export function handleCreateIncreaseOrder(event: CreateIncreaseOrder): void {
     } else {
         account.lastOrderIndex = event.params.orderIndex
         let newOrdersList = account.orders
-        let newOrder = account.orders
 
         //use address concat lastOrderIndex as uuid
         let order = new Order(event.params.account.concatI32(event.params.orderIndex.toI32()))
@@ -161,17 +161,144 @@ export function handleExecuteIncreaseOrder(event: ExecuteIncreaseOrder): void {
 }
 
 
-// handle DecreaseOrder
+// ========================================================handle DecreaseOrder========================================================
 export function handleCreateDecreaseOrder(event: CreateDecreaseOrder): void {
+    let account = Account.load(event.params.account)
+    if (account === null) {
+        account = new Account(event.params.account)
+        account.address = event.params.account.toHexString()
+        account.lastOrderIndex = event.params.orderIndex
+        account.orders = []
+        account.deOrders = []
+
+        let newOrdersList = account.deOrders
+        //use address concat lastOrderIndex as uuid
+        let order = new DeOrder(event.params.account.concatI32(event.params.orderIndex.toI32()))
+        order.orderOwner = account.id
+        order.ownerAddress = account.address
+        order.orderIndex = event.params.orderIndex
+        order.collateralToken = event.params.collateralToken.toHexString()
+        order.collateralDelta = event.params.collateralDelta
+        order.indexTokenAddress = event.params.indexToken.toHexString()
+        order.sizeDelta = event.params.sizeDelta
+        order.isLong = event.params.isLong
+        order.triggerPrice = event.params.triggerPrice.toString()
+        order.triggerAboveThreshold = event.params.triggerAboveThreshold
+        order.executionFee = event.params.executionFee.toString()
+        order.excuted = false
+        order.save()
+
+        newOrdersList.push(order.id)
+        account.deOrders = newOrdersList
+        account.save()
+
+    } else {
+        account.lastOrderIndex = event.params.orderIndex
+        let newOrdersList = account.deOrders
+
+        //use address concat lastOrderIndex as uuid
+        let order = new DeOrder(event.params.account.concatI32(event.params.orderIndex.toI32()))
+        order.orderOwner = account.id
+        order.ownerAddress = account.address
+        order.orderIndex = event.params.orderIndex
+        order.collateralToken = event.params.collateralToken.toHexString()
+        order.collateralDelta = event.params.collateralDelta
+        order.indexTokenAddress = event.params.indexToken.toHexString()
+        order.sizeDelta = event.params.sizeDelta
+        order.isLong = event.params.isLong
+        order.triggerPrice = event.params.triggerPrice.toString()
+        order.triggerAboveThreshold = event.params.triggerAboveThreshold
+        order.executionFee = event.params.executionFee.toString()
+        order.excuted = false
+        order.save()
+
+        newOrdersList.push(order.id)
+        account.deOrders = newOrdersList
+        account.save()
+
+    }
+
 }
 
 export function handleUpdateDecreaseOrder(event: UpdateDecreaseOrder): void {
+
+    let address = event.params.account
+    let orderIdex = event.params.orderIndex
+
+
+    let account = Account.load(address)
+    if (account !== null) {
+        let orderList = account.deOrders
+        //find order of event.params.orderIndex and remove it
+        for (let i = 0; i < orderList.length; i++) {
+            let iOrder = DeOrder.load(orderList[i])
+            if (iOrder !== null) {
+                if (iOrder.orderIndex.equals(orderIdex)) {
+                    //set excuted to true
+                    iOrder.triggerPrice = event.params.triggerPrice.toString()
+                    iOrder.triggerAboveThreshold = event.params.triggerAboveThreshold
+                    iOrder.sizeDelta = event.params.sizeDelta
+                    iOrder.collateralDelta = event.params.collateralDelta
+                    log.debug("iOrder Id: {} , excuted:", [iOrder.id.toHexString(), iOrder.excuted.toString()])
+                    iOrder.save()
+                }
+            }
+        }
+
+    }
+
 }
 
 export function handleCancelDecreaseOrder(event: CancelDecreaseOrder): void {
+
+    let address = event.params.account
+    let orderIdex = event.params.orderIndex
+
+
+    let account = Account.load(address)
+    if (account !== null) {
+        let orderList = account.deOrders
+        //find order of event.params.orderIndex and remove it
+        for (let i = 0; i < orderList.length; i++) {
+            let iOrder = DeOrder.load(orderList[i])
+            if (iOrder !== null) {
+                if (iOrder.orderIndex.equals(orderIdex)) {
+                    //set excuted to true
+                    iOrder.excuted = true
+                    log.debug("iOrder Id: {} , excuted:", [iOrder.id.toHexString(), iOrder.excuted.toString()])
+                    iOrder.save()
+                }
+            }
+        }
+
+    }
+
 }
 
 export function handleExecuteDecreaseOrder(event: ExecuteDecreaseOrder): void {
+
+    let address = event.params.account
+    let orderIdex = event.params.orderIndex
+
+
+    let account = Account.load(address)
+    if (account !== null) {
+        let orderList = account.deOrders
+        //find order of event.params.orderIndex and remove it
+        for (let i = 0; i < orderList.length; i++) {
+            let iOrder = DeOrder.load(orderList[i])
+            if (iOrder !== null) {
+                if (iOrder.orderIndex.equals(orderIdex)) {
+                    //set excuted to true
+                    iOrder.excuted = true
+                    log.debug("iOrder Id: {} , excuted:", [iOrder.id.toHexString(), iOrder.excuted.toString()])
+                    iOrder.save()
+                }
+            }
+        }
+
+    }
+
 }
 
 
